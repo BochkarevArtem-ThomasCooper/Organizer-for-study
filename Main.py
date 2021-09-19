519be1e477fd5325c4874cfe75813a99f5cbabf9
unknown <BochkarevArtem-ThomasCooper>
2021-09-19 22:53:14 +0300

    Tasks for the week set

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
 # Main
+
+# Things TO DO
+# 1. Get "Dear PyGUI" and get known with it.
+# 2. Fresh up SQLite3 library knolege:
+# - How to store a tuple object it chart (in one cell)
+# - How to store a dictionary object in chart (in one cell)
+# 3. Think of a way to encrypt and decrypt text data in a simpe but efficient way.
+# 4. Finde a way to recive and use system paths to provide ability of calling for a needed book right from the app.
+# 5. Follow PEP8 standard.
+# 6. Provide documentation for the code.
