8032af7605a7a8c859dfee8dba3b35c464aabaf8
unknown <BochkarevArtem-ThomasCooper>
2021-09-22 23:41:21 +0300

    Firs marks of a future main class.

---
+++ Class_Title.py
@@ -0,0 +1,17 @@
+#Class Title
+
+import PySimpleGUI as sg
+
+sg.theme('DarkBlack1')
+
+class Title: # That class represents notes for one studeing metter
+
+    def __init__(self,titleName, spentTime):
+        self.titleName = titleName
+        self.spentTime = spentTime
+
+    #gef get_data_from_db(self): # That function is in control of reading data from the data base
+
+    #def appirance(self): # That function returnes the look of the note
+
+    #def save_result(self): # That function updates data in datdbase.
