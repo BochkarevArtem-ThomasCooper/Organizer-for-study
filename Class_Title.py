9bf7c0baafabc16cf5601f95ca3fd641b5ad7705
unknown <BochkarevArtem-ThomasCooper>
2021-09-29 22:16:58 +0300

    wroute a cople funcs

--- Class_Title.py
+++ Class_Title.py
@@ -2,16 +2,32 @@
 
 import PySimpleGUI as sg
 
+import DBServer as DB
+
 sg.theme('DarkBlack1')
 
 class Title: # That class represents notes for one studeing metter
 
-    def __init__(self,titleName, spentTime):
+    def __init__(self, titleName, user = 'user',*spentTime):
         self.titleName = titleName
+        self.user = user
         self.spentTime = spentTime
 
     #gef get_data_from_db(self): # That function is in control of reading data from the data base
 
-    #def appirance(self): # That function returnes the look of the note
+    def appirance(self): # That function returnes the look of the note
+
+        topic = 'Notes for: ' + self.titleName
+        timeInvolved = f'Studyed for {self.spentTime[0]} days, {self.spentTime[1]} hours and {self.spentTime[2]} minutes.'
+
+        litTableName = DB.input_modifier(self.titleName, self.user)+'topicTitles'
+        litDict = {}
+        litDict = DB.get_literature_dict(litTableName) # !! We need a func that going to resive title-name and open exact file. So title-name is 'key' and path to the file is 'value'
+        
+        layout = [
+            [sg.Text(Text =topic, auto_size_text=True)],
+            [sg.Text(Text =timeInvolved, auto_size_text=True)],
+            [sg.Combo(DB.get_literature_list(litTableName), bind_return_key=True, enable_events=True, readonly=True, k='-COMBO-')] # here sould be added a dictionary tipe object, created from Literature table 
+            ]
 
     #def save_result(self): # That function updates data in datdbase.
--- DBServer.py
+++ DBServer.py
@@ -3,12 +3,47 @@
 import sqlite3
 import PySimpleGUI as sg
 
+sg.theme('DarkBlack1')
+
 con = sqlite3.connect('Study_Notes.db')
 cur = con.cursor()
 
-def first_run(): # That func. should be runned if first run detected.
+def input_modifier(title, user='user'): # func to make user input robust   
+    safeName = title.replace(' ','_') + user
+    return safeName
+
+
+def first_run(): # That func. creates the main table we use to store our users login information.
  
     try:
-        cur.execute('CREATE TABLE PasswordTable (User text, Password text)')
+        cur.execute('CREATE TABLE if not exists PasswordTable (User text, Password text)')
     except Exception as e:
-        sg.popup('Unexpected exception!', e)
+        sg.popup('Unexpected exception!\n', e)
+
+def new_title(title, user='user'): # Creates tables to store information about new title, studyng by user. 
+
+    try:
+        cur.execute('CREATE TABLE if not exists %s (User text, Password text)' %(input_modifier(title, user)))
+        cur.execute('CREATE TABLE if not exists %s (Date text, Note text)' %(input_modifier(title, user)+'topicLog'))
+        cur.execute('CREATE TABLE if not exists %s (TiteName text, Path text)' %(input_modifier(title, user)+'topicTitles'))
+        con.commit()
+    except Exception as e:
+        sg.popup('Unexpected exception!\n', e)
+
+def get_literature_dict(tablename): # making a dictionaty object from table
+    litDict = {}
+    try:
+        for name, path in cur.execute(f'SELECT * FROM {tablename}'):
+            litDict[name] = path
+    except Exception as e:
+        sg.popup('Unexpected exception!\n', e)
+    return litDict
+
+def get_literature_list(tablename): # making a list object with title names onely, from table
+    litList = []
+    try:
+        for name in cur.execute(f'SELECT TiteName FROM {tablename}'):
+            litList.append(name)
+    except Exception as e:
+        sg.popup('Unexpected exception!\n', e)
+    return litList
